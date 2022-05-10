71dc0fcaa7189c53dd39393fb950e31e2e8f3ea5
Lili <resina2017@gmail.com>
2022-05-10 20:37:32 +0800

    Planet.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,2 +1,30 @@
 public class NBody {
+    public static double readRadius(String filename) {
+        In in = new In(filename);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+
+    public static Planet[] readPlanets(String filename) {
+        In in = new In(filename);
+        int num = in.readInt();
+        double radius = in.readDouble();
+
+        Planet[] planets = new Planet[num];
+
+        for (int i = 0; i < num; i++){
+            double x = in.readDouble();
+            double y = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgName = in.readString();
+
+            Planet a = new Planet(x,y,xxVel,yyVel,mass,imgName);
+            planets[i] = a;
+        }
+        return planets;
+    }
+
 }
